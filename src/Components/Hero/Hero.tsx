25d384fd14a006ff1bcc4213daa19326781a3bec
Rohit-vunet <rohitkg@vunetsystems.com>
2025-02-07 15:59:03 +0530

    Hero section complete

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Hero.css';
+import Heart from '../../assets/heart.png';
+import hero_image_back from '../../assets/hero_image_back.png';
+import hero_image from '../../assets/hero_image.png';
+import Calories from '../../assets/calories.png';
 
 const Hero = () => {
   return (
@@ -43,9 +47,32 @@ const Hero = () => {
     <span>expert coaches</span>
   </div>
 </div>
+
+<div className='hero-buttons'>
+  <button className = "btn">Get Started</button>
+  <button className = "btn">Learn More</button>
 </div>
-      <div className="right-h">Right Side
+
+
+</div>
+      <div className="right-h">
+        <button className='btn'>Join Now</button>
+        <img src = {Heart} alt = " "/>
+        <span>Heart Rate</span>
+        <span>11bpm</span>
       </div>
+
+
+      <img src= {hero_image} alt= "" className='hero-image'/>
+      <img src ={hero_image_back} alt = " " className='hero-image-back'/>
+
+ <div className='calories'>
+  < img src= {Calories} alt = ""/>
+  <span>Calories Burned</span>
+  <span>220kcal</span>
+
+ </div>
+
     </div>
   );
 };

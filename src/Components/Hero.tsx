5cdd31f1d643391c7695ec23e7b5639ca996cb04
Rohit-vunet <rohitkg@vunetsystems.com>
2025-02-07 11:55:51 +0530

    Gym website

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className="Hero">
+        <div className="Left-h">left-side</div>
+        <div className="right-h">right-side</div>
+    </div>
+  )
+}
+
+export default Hero
